10ce65f37710dc0bd99f7c35194be0dbf7206e9f
Szymon Kazmierczak <251546@edu.p.lodz.pl>
2025-03-18 20:40:34 +0100

    Reprezentacja kulki w warstwie danych, blad przy kompilacji testów

---
+++ Data/BallRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media.Media3D;
+
+namespace proj.Data
+{
+    public class BallRepository
+    {
+        private List<Ball> balls = [];
+
+        public void AddBall(Ball ball) => balls.Add(ball);
+        public void RemoveBall(Ball ball) => balls.Remove(ball);
+        public List<Ball> GetAllBalls() => new(balls);
+
+        public void Clear()
+        {
+            foreach (var ball in balls)
+            {
+                RemoveBall(ball);
+            }
+        }
+    }
+
+}
+
--- Data/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Data
-{
-    class Class1
-    {
-
-    }
-}
